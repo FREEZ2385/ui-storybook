079e810073b556b4735c19ac77b3258ac9db9c7b
FREEZ2385 <schlee9703@naver.com>
2020-10-13 22:33:14 +0900

    [Update] change readme.md

--- src/component/Atoms/ButtonComponent.js
+++ src/component/Atoms/ButtonComponent.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import { Button } from '@material-ui/core';
 
 const buttonStyle = makeStyles((theme) => ({
     large: {
       width: 256,
-      marginRight: 20,
     },
-    middle: {
+    medium: {
       width: 164,
-      marginRight: 20,
     },
     small: {
       width: 75,
-      marginRight: 20,
     },
   }));
 
@@ -22,8 +20,14 @@ export const ButtonComponent = ({ ...props }) => {
     const sizeStyle = buttonStyle();
 
     return (
-        <Button variant="contained" color="Secondary" className={sizeStyle[size]} onClick={onClick}>
+        <Button variant="contained" style={{ borderRadius: 20,marginRight: 20, padding: '10px 5% ',}} color="Secondary" className={sizeStyle[size]} onClick={onClick}>
         {text}
         </Button>
         );
 }
+
+// ButtonComponent.PropTypes = {
+//   text: PropTypes.string,
+//   size: PropTypes.oneOf(['small', 'medium', 'large']),
+//   onClick: PropTypes.func,
+// };
